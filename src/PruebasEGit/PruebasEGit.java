82ae887df648af3dbc04d4c6ba20c10b68f96bc4
DaniBuiles <danibuiles@gmail.com>
2025-03-07 13:06:03 +0100

    Primer commit desde rama1

--- src/PruebasEGit/PruebasEGit.java
+++ src/PruebasEGit/PruebasEGit.java
@@ -5,6 +5,7 @@ public class PruebasEGit {
 	public static void main(String[] args) {
 		System.out.println("Pruebas EGit");
 		System.out.println("Primer Cambio");
+		System.out.println("Cambio desde rama1");
 		
 	}
 
